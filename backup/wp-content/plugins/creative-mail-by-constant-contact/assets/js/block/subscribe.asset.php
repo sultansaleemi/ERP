aaa84ffa7623e2ec553c837505dcf86be21a899f
sultansaleemi <153021484+sultansaleemi@users.noreply.github.com>
2025-05-12 11:57:43 +0500

    Delete backup directory

--- backup/wp-content/plugins/creative-mail-by-constant-contact/assets/js/block/subscribe.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '986ecdd0e4b7dee76254c227b105ebca');
--- backup/wp-content/plugins/wp-plugin-hostgator/vendor/newfold-labs/wp-module-coming-soon/build/1.3.6/coming-soon.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2881219076ba99d271c3');

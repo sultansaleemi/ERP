f084b7d7528f995a191353c506b7c2e6960a2635
sultansaleemi <153021484+sultansaleemi@users.noreply.github.com>
2025-06-03 13:46:44 +0500

    Update index.blade.php

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -6,13 +6,28 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h3>Files</h3>
+                  @php
+                  $type_name = '';
+                    if(request('type') == 1){
+                      $type_name = App\Models\Riders::find(request('type'))?->value('name')??'';
+                    }
+                    if(request('type') == 2){
+                      $type_name = App\Models\Bikes::find(request('type'))?->value('plate')??'';
+                    }
+
+                  @endphp
+                    <h3>{{$type_name}} Files</h3>
                 </div>
                 <div class="col-sm-6">
-                    <a class="btn btn-primary show-modal action-btn"
-                       href="javascript:void(0);" data-action="{{ route('files.create','rider_id',request()->segment(2)) }}" data-size="sm" data-title="Upload Document">
-                        Add New
-                    </a>
+                  <a class="btn btn-default action-btn mx-2"
+                  href="{{url()->previous() }}">
+                  <i class="fa fa-arrow-left"></i> &nbsp;Back
+               </a>
+                  <a class="btn btn-primary show-modal action-btn"
+                  href="javascript:void(0);" data-action="{{ route('files.create',['type_id'=>request('type_id')??1,'type'=>request('type')??1]) }}" data-size="sm" data-title="Upload File">
+                   Upload File
+               </a>
+
                 </div>
             </div>
         </div>

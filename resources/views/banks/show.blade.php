5168e273a140c4f08945a1a810c4eb76de69ea5d
soft1986 <softtec1986@gmail.com>
2025-06-14 13:55:08 +0500

    banks module updating

--- resources/views/banks/show.blade.php
+++ resources/views/banks/show.blade.php
@@ -1,4 +1,9 @@
+@extends('suppliers.view')
 
+
+@section('page_content')
                 <div class="row">
+                    
                     @include('banks.show_fields')
                 </div>
+@endsection
---
+++ resources/views/banks/view.blade.php
@@ -0,0 +1,24 @@
+<!-- resources/views/suppliers/view.blade.php -->
+@extends('layouts.app')
+@section('title', 'Banks Profile')
+
+@section('content')
+<div class="row">
+
+    
+    <div class="col-xl-12 col-md-9 col-lg-7 order-0 order-md-1">
+        <div class="nav-align-top">
+          <ul class="nav nav-pills flex-column flex-md-row flex-wrap mb-3 row-gap-2">
+            <li class="nav-item"><a class="nav-link @if(Request::is('suppliers/show/*') || Request::route()->getName() === 'banks.show') active @endif" href="@isset($banks->id){{ route('banks.show', $banks->id) }}@else#@endif"><i class="ti ti-user-check ti-sm me-1_5"></i>Information</a></li>
+            @isset($banks)
+            <li class="nav-item"><a class="nav-link @if(request()->is('banks/ledger/' . $banks->id)) active @endif" href="{{ route('banks.ledger', $banks->id) }}"><i class="ti ti-file ti-sm me-1_5"></i>Ledger</a></li>
+            @endisset
+          </ul>
+        </div>
+
+        <div class="card mb-5" id="cardBody">
+          @yield('page_content')
+        </div>
+    </div>
+</div>
+@endsection

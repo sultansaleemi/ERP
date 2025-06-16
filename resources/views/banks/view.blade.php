d2c44df2335eb15154ce3f56da1c02eb9637b2ad
soft1986 <softtec1986@gmail.com>
2025-06-16 14:54:41 +0500

    fines module links

--- resources/views/banks/view.blade.php
+++ resources/views/banks/view.blade.php
@@ -11,7 +11,7 @@
           <ul class="nav nav-pills flex-column flex-md-row flex-wrap mb-3 row-gap-2">
             <li class="nav-item"><a class="nav-link @if(Request::is('suppliers/show/*') || Request::route()->getName() === 'banks.show') active @endif" href="@isset($banks->id){{ route('banks.show', $banks->id) }}@else#@endif"><i class="ti ti-user-check ti-sm me-1_5"></i>Information</a></li>
             @isset($banks)
-            <li class="nav-item"><a class="nav-link @if(request()->is('banks/ledger/' . $banks->id)) active @endif" href="{{ route('banks.ledger', $banks->id) }}"><i class="ti ti-file ti-sm me-1_5"></i>Ledger</a></li>
+            <li class="nav-item"><a class="nav-link @if(request()->segment(3) == 'ledger') active @endif" href="{{ route('banks.ledger', $banks->id) }}"><i class="ti ti-file ti-sm me-1_5"></i>Ledger</a></li>
             @endisset
 
             <li class="nav-item">
